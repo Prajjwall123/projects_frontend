89552075e58ae30f452417cd895b1b80eec36d92
Prajjwall123 <pokhrelprajwal29@gmail.com>
2024-12-30 22:25:54 +0545

    Card/01

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+        searchar
+    );
+};
+
+export default SearchBar;
--- src/core/public/home.jsx
+++ src/core/public/home.jsx
@@ -2,12 +2,29 @@ import React from 'react';
 import Navbar from '../../components/navbar';
 import Hero from '../../components/hero';
 import Footer from '../../components/footer';
+import Card from '../../components/card';
+import SearchBar from '../../components/SearchBar';
+
 
 function Home() {
     return (
         <>
             <Navbar />
             <Hero />
+            <div className="text-5xl font-sans font-bold bg-base-200 text-center">
+                Explore Projects:
+            </div>
+            <SearchBar />
+            <div className="bg-base-200 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+                <div><Card /></div>
+                <div><Card /></div>
+                <div><Card /></div>
+
+                <div><Card /></div>
+                <div><Card /></div>
+                <div><Card /></div>
+            </div>
+
             <Footer />
         </>
     );
