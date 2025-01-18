43b9d2ec6c98c62f3752a3e88a5dac1bbdd74883
Prajjwall123 <pokhrelprajwal29@gmail.com>
2025-01-18 15:35:40 +0545

    API and Helpers/01

---
+++ src/core/utils/authHelpers.jsx
@@ -0,0 +1,33 @@
+import API from './api';
+
+const loginUser = async (credentials) => {
+    try {
+        const response = await API.post("/auth/login", credentials);
+        return response.data;
+    } catch (error) {
+        throw error.response ? error.response.data : { message: "Network error" };
+    }
+};
+
+const registerUser = async (userData) => {
+    try {
+        const response = await API.post("/auth/register", userData);
+        return response.data;
+    } catch (error) {
+        console.error(error.response ? error.response.data : error.message);
+        throw error.response ? error.response.data : { message: "Network error" };
+    }
+};
+
+
+
+const verifyOTP = async (payload) => {
+    try {
+        const response = await API.post("/auth/verify-otp", payload);
+        return response.data;
+    } catch (error) {
+        throw error.response ? error.response.data : { message: "Network error" };
+    }
+};
+
+export { loginUser, registerUser, verifyOTP };
